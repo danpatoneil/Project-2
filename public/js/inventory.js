ac97cc398fc44b8f82200e1307a494164995360b
Katie Bug <katierosealford@gmail.com>
2024-05-16 19:53:16 -0400

    added button to add item modal, formatted js in inventory.js for modals

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -2,11 +2,13 @@
 const gamesButton = document.querySelector('#gamesModalButton');
 const consoleButton = document.querySelector('#consoleModalButton');
 const controllersButton = document.querySelector('#controllersModalButton');
+const addItemButton = document.querySelector('#addItemModalButton');
 
 // Get references to the modals
 const gamesModal = document.querySelector('#gamesModal');
 const consoleModal = document.querySelector('#consoleModal');
 const controllersModal = document.querySelector('#controllersModal');
+const addItemModal = document.querySelector('#addItemModal');
 
 // Add event listeners to the buttons
 gamesButton.addEventListener('click', () => {
@@ -21,10 +23,15 @@ controllersButton.addEventListener('click', () => {
   controllersModal.style.display = 'block';
 });
 
+addItemButton.addEventListener('click', () => {
+  addItemModal.style.display = 'block';
+});
+
 // Get references to the close buttons
 const closeButtonGames = document.querySelector('#gamesModal .close');
 const closeButtonConsole = document.querySelector('#consoleModal .close');
 const closeButtonControllers = document.querySelector('#controllersModal .close');
+const closeButtonAddItem = document.querySelector('#addItemModal .close');
 
 // Add event listeners to the close buttons
 closeButtonGames.addEventListener('click', () => {
@@ -37,5 +44,9 @@ closeButtonConsole.addEventListener('click', () => {
 
 closeButtonControllers.addEventListener('click', () => {
   controllersModal.style.display = 'none';
+});
+
+closeButtonAddItem.addEventListener('click', () => {
+  addItemModal.style.display = 'none';
 });
 
